9f70ecac1f14377a712ab764c757981160cc0fa2
Celeste Glavin <cmugla@gmail.com>
2016-08-09 15:15:18 -0400

    build out api model for converting lat long to zip

--- farmer_ios/models/apiData.js
+++ farmer_ios/models/apiData.js
@@ -3,7 +3,6 @@ const request = require('request');
 module.exports = {
 
   getData(req,res,next){
-   let countryCode = req.params.code
     request({
       url:'https://data.ny.gov/resource/farmersmarkets.json',
       method:'get',
@@ -15,6 +14,29 @@ module.exports = {
     },(err,response,body)=>{
       if(err) throw err;
       res.results = body;
+      next();
+    })
+  },
+
+  getZip(req,res,next){
+    let latlng = `${req.query.latitude},${req.query.longitude}`
+    console.log(latlng)
+    request({
+      url:'http://maps.googleapis.com/maps/api/geocode/json',
+      method:'get',
+      json:true,
+      qs: {
+        "latlng": latlng
+      }
+    }, (err, response, body)=>{
+      if(err) throw err;
+      console.log(body)
+      let addressComponents = body.results[0].address_components
+      addressComponents.map((component)=>{
+        if(component.types.includes("postal_code")) {
+          res.results = component.short_name;
+        }
+      })
       next();
     })
   }
--- farmer_ios/routes/mkts.js
+++ farmer_ios/routes/mkts.js
@@ -1,6 +1,10 @@
 const mktRouter = require('express').Router();
 const apiData   = require('../models/apiData');
 
+mktRouter.get('/location', apiData.getZip, (req,res)=>{
+  res.json(res.results)
+})
+
 mktRouter.get('/', apiData.getData, (req, res)=>{
   res.json(res.results)
 })
